de24028dc44f70fbc476e6031150fabdab4a8b8c
Stephen Shin <skfk@login-2.zaratan.umd.edu>
2024-03-03 19:08:27 -0500

    list of packages

---
+++ BMM/packages.sh
@@ -0,0 +1,22 @@
+mkdir packages
+cd packages
+curl https://cran.r-project.org/src/contrib/loo_2.7.0.tar.gz --output loo_2.7.0.tar.gz
+curl https://cran.r-project.org/src/contrib/inline_0.3.19.tar.gz --output inline_0.3.19.tar.gz
+curl https://cran.r-project.org/src/contrib/QuickJSR_1.1.3.tar.gz --output QuickJSR_1.1.3.tar.gz
+curl https://cran.r-project.org/src/contrib/StanHeaders_2.32.6.tar.gz --output StanHeaders_2.32.6.tar.gz
+curl https://cran.r-project.org/src/contrib/colourpicker_1.3.0.tar.gz --output colourpicker_1.3.0.tar.gz
+curl https://cran.r-project.org/src/contrib/threejs_0.3.3.tar.gz --output threejs_0.3.3.tar.gz
+curl https://cran.r-project.org/src/contrib/dygraphs_1.1.1.6.tar.gz --output dygraphs_1.1.6.tar.gz
+curl https://cran.r-project.org/src/contrib/shinythemes_1.2.0.tar.gz --output shinythemes_1.2.0.tar.gz
+curl https://cran.r-project.org/src/contrib/Brobdingnag_1.2-9.tar.gz --output Brobdingnag_1.2-9.tar.gz
+curl https://cran.r-project.org/src/contrib/RcppEigen_0.3.4.0.0.tar.gz --output RcppEigen_0.3.4.0.0.tar.gz
+curl https://cran.r-project.org/src/contrib/Matrix_1.6-5.tar.gz --output Matrix_1.6-5.tar.gz
+curl https://cran.r-project.org/src/contrib/ggplot2_3.5.0.tar.gz --output ggplot2_3.5.0.tar.gz
+curl https://cran.r-project.org/src/contrib/rstan_2.32.5.tar.gz --output rstan_2.32.5.tar.gz
+curl https://cran.r-project.org/src/contrib/rstantools_2.4.0.tar.gz --output rstantools_2.4.0.tar.gz
+curl https://cran.r-project.org/src/contrib/bayesplot_1.11.1.tar.gz --output bayesplot_1.11.1.tar.gz
+curl https://cran.r-project.org/src/contrib/shinystan_2.6.0.tar.gz --output shinystan_2.6.0.tar.gz
+curl https://cran.r-project.org/src/contrib/bridgesampling_1.1-2.tar.gz --output bridgesampling_1.1-2.tar.gz
+curl https://cran.r-project.org/src/contrib/nleqslv_3.3.5.tar.gz --output nleqslv_3.3.5.tar.gz
+curl https://cran.r-project.org/src/contrib/brms_2.20.4.tar.gz --output brms_2.20.4.tar.gz
+cd ../
